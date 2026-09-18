14b59cd21fc48ea12ae2b6427a52d0ef2115e3b6
tuturam <ahmadrama251005@gmail.com>
2026-09-19 00:04:28 +0700

    refactor: languages to json, gist already not found

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,7 +22,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const dataLanguage = async () => {
   try {
-    const response = await fetch("https://raw.githubusercontent.com/kamranahmedse/githunt/master/src/components/filters/language-filter/languages.json")
+    const response = await fetch("languages.json")
     const data = await response.json()
     return data
   } catch (error) {
